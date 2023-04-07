b55f427d362a8b46ea8fc407beb1b41f3f3ef4a3
3lielFernandes <123779747+elielfernandes333@users.noreply.github.com>
2023-04-07 17:02:07 -0300

    Create celsiusToFahrenheitAndFahrenheitToCelsius.py

---
+++ celsiusToFahrenheitAndFahrenheitToCelsius.py
@@ -0,0 +1,11 @@
+# celsius to fahrenheit
+
+celsius = float(input('Digite a temperatura em Celsius: '))
+f = 9 * celsius/ 5 + 32
+print(f'{celsius} Celsius é igual a {f} Fahrenheit')
+
+# fajrenheit to celsius
+
+fahrenheit = float(input('Digite a temperatura em Fahrenheit: '))
+c = (fahrenheit - 32) * 5 / 9
+print(f'{fahrenheit} Fahrenheit é igual a {c} Celsius')
