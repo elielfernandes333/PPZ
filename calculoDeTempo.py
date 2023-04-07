9b5cc6af9b07fa346c2b4a0bf94bcfc3605d396c
3lielFernandes <123779747+elielfernandes333@users.noreply.github.com>
2023-04-07 17:00:48 -0300

    Create calculoDeTempo.py

---
+++ calculoDeTempo.py
@@ -0,0 +1,5 @@
+distancia = float(input('digite a distancia a percorrer em km: '))
+velocidadeMedia = int(input('digite a velocidade media em km/h: '))
+tempo = distancia / velocidadeMedia 
+
+print(f'o tempo da viagem será de: {tempo} horas')
